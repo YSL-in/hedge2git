f24c8f40d447030230c19c22bd314d3b1632ecee
YSL <linyusung1998@gmail.com>
2024-08-18 18:25:53 +0800

    Add CLI options

---
+++ _helpers.py
@@ -0,0 +1,44 @@
+import sys
+import typing as t
+
+import click
+
+
+def validate(**actions: t.Mapping) -> None:
+    def exit_with_error(msg: str) -> t.NoReturn:
+        click.UsageError(msg, click.get_current_context()).show(sys.stderr)
+        exit(1)
+
+    if actions['pull'] is not None and actions['push'] is not None:
+        exit_with_error("Got both 'pull' and 'push'")
+    pass
+
+
+def pull(branch: str) -> None:
+    """It should be called each time
+    TODO:
+    - create the temp repo if not exists
+    - pull the latest changes
+    - checkout to an orphan branch
+        - remove .git/index
+        - clear all files
+    - extract the notes from hedgedoc database
+    - merge the orphan (--allow-unrelated-history --ff-only)
+        - prompt to continue to solve conflict if any
+    """
+    if branch is None:
+        return
+
+
+def push(comment: str, flatten: bool) -> None:
+    """
+    TODO:
+    - create the temp repo if not exists
+    - pull the latest changes
+    - extract the notes from hedgedoc database
+    - add changes to working tree where hierarchy is optionally built according to tags (order matters!)
+    - commit changes with an optional message
+    - push changes
+    """
+    if comment is None:
+        return
---
+++ hedge2git.py
@@ -0,0 +1,42 @@
+import typing as t
+from datetime import datetime
+
+import click
+
+import _helpers
+
+
+@click.command()
+@click.option(
+    '--pull', '--download', 'pull', metavar='BRANCH', type=str,
+    is_flag=False, flag_value='main',  # TODO: make it configurable
+    default=None, show_default=False,
+    help='Pull the latest changes.',
+)
+@click.option(
+    '--push', '--upload', 'push', metavar='COMMENT',
+    is_flag=False, flag_value=datetime.now().strftime('Pushed at %Y-%m-%d %H:%M:%S'),
+    default=None, show_default=True,
+    help='Push changes.',
+)
+@click.option(
+    '--flatten',
+    is_flag=True,
+    default=False, show_default=True,
+    help='Build hierarchy based on tags.',
+)
+def hedge2git(**actions: t.Mapping):
+    """
+    Sync hedgedoc via Git registries. Use .env to configure repository, access token, etc.
+    """
+    _helpers.validate(**actions)
+
+    if (branch := actions['pull']) is not None:
+        _helpers.pull(branch)  # type: ignore
+
+    if (comment := actions['push']) is not None:
+        _helpers.push(comment, actions['flatten'])  # type: ignore
+
+
+if __name__ == '__main__':
+    hedge2git()
