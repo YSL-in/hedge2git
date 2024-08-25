b9b54d4545ca5952be2cd98d7394c580212469ed
YSL <linyusung1998@gmail.com>
2024-08-25 22:54:01 +0800

    Add git interface for push

--- _helpers.py
+++ _helpers.py
@@ -3,10 +3,13 @@ import typing as t
 from functools import reduce
 from pathlib import Path
 
+import git
+import git.types
 from sqlalchemy import Column
 
 from configs import configs
-from hedgedoc.core import hedgedoc_store
+from hedgedoc import hedgedoc, hedgedoc_store
+from hedgedoc.models import Note
 from utils import exit_with_error
 
 
@@ -18,33 +21,48 @@ def validate(**actions: t.Mapping) -> None:
 def pull(branch: str | None) -> None:
     """It should be called each time
     TODO:
-    - create the temp repo if not exists
-    - pull the latest changes
+    v create the temp repo if not exists
+    v pull the latest changes
     - checkout to an orphan branch
         - remove .git/index
         - clear all files
     - extract the notes from hedgedoc database
     - merge the orphan (--allow-unrelated-history --ff-only)
         - prompt to continue to solve conflict if any
+    - update the hedgedoc database with the latest notes
+        - add new notes to browsing history
     """
     if branch is None:
         return
+
+    print(f'{hedgedoc.get_history() = }')
+    note: Note = hedgedoc_store.session.query(Note).filter(Note.id == '9d2ed746-5fe1-48c3-8e2f-1c683b6e9bb9').first()
+    hedgedoc.add_history(note)
 
 
 def push(comment: str | None) -> None:
     """
     TODO:
-    - create the temp repo if not exists
-    - pull the latest changes
-    - extract the notes from hedgedoc database
-    - add changes to working tree where hierarchy is built according to tags
-    - commit changes with an optional message
-    - push changes
+    v create the temp repo if not exists
+    v pull the latest changes
+    v extract the notes from hedgedoc database
+    v add changes to working tree where hierarchy is built according to tags
+    v commit changes with an optional message
+    v push changes
     """
     if comment is None:
         return
 
     sync_path = configs['SYNC_PATH']
+    repo = git.Repo.init(sync_path)
+    origin = repo.create_remote('origin', configs['GIT_REPO'])
+    try:
+        origin.fetch()
+    except git.GitCommandError:
+        exit_with_error(f"Invalid git repository: {configs['GIT_REPO']}")
+    origin.pull()
+
+    notes = []
     for note in hedgedoc_store.get_notes(owner=hedgedoc_store.get_current_user()):
         if not note.content:  # type: ignore
             continue
@@ -52,7 +70,15 @@ def push(comment: str | None) -> None:
         # TODO: add confliction avoidance
         path = reduce(lambda p, part: p / part, note.tags, sync_path)
         name = note.title or re.split(r'\s', note.content, maxsplit=1)[0]  # type: ignore
-        _write_file(path / f'{name}.md', note.content)
+        notes.append(path / f'{name}.md')
+        _write_file(notes[-1], note.content)
+
+    # TODO: add dry-run mode & show dirty files before committing
+    # TODO: create primary branch if none exists
+    author = git.Actor(configs['GIT_USER'], configs['GIT_EMAIL'])
+    repo.index.add(notes)
+    repo.index.commit(comment, author=author, committer=author)
+    origin.push().raise_if_error()
 
 
 def _write_file(path: Path, content: str | Column[str]) -> None:
